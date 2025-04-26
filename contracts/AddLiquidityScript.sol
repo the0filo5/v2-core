11ad29a79c4988e832a26bf3188a36c7bd79401c
the0filo5 <theo@constantinidis.com>
2025-04-26 18:38:14 +0100

    TRied Running liquidity Script In Solidity within REMIX

--- contracts/AddLiquidityScript.sol
+++ contracts/AddLiquidityScript.sol
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16;
+pragma solidity =0.5.16;
 
-import "./UniswapV2Pair.sol";
-import "TheosTestToken.sol"; // Your actual contract import for token0
-import "TheosStableToken.sol";  //Your Actual Contract Import For Token1
+import "./UniswapV2Pair.sol";   //contracts/UniswapV2Pair.sol
+import "./interfaces/IUniswapV2Pair.sol";
+import './UniswapV2ERC20.sol';
+import './libraries/Math.sol';
+import './libraries/SafeMath.sol';
+import './libraries/UQ112x112.sol';
+import './interfaces/IERC20.sol';
+import './interfaces/IUniswapV2Factory.sol';
+import './interfaces/IUniswapV2Callee.sol';
+import "https://raw.githubusercontent.com/the0filo5/CryptoCoins/main/Theos StableCoin.sol";
+import "https://raw.githubusercontent.com/the0filo5/CryptoCoins/main/contract-4bee95a821.sol";
+
 
 contract AddLiquidityScript {
-    TheosTestToken public token0;
-    TheosStableToken public token1; 
+    TheosTestCoin public token0;
+    TheosStableCoin public token1; 
     UniswapV2Pair public pair;
 
     constructor() public {
 
-        token0 = address('0x88a2c78bd1AA32B6753636e3fCdA896e4E0c549C');  
-        token1 = address('0x3d94e6291a3D00fb21986811a326fD5c0ec5e7c3');
+        token0 = TheosTestCoin(address('0x88a2c78bd1AA32B6753636e3fCdA896e4E0c549C'));  
+        token1 = TheosStableCoin(address('0x3d94e6291a3D00fb21986811a326fD5c0ec5e7c3'));
         pair = UniswapV2Pair(address('0xB065e914155eFbfEbC0A19da541E3eb05Ae371C6'));
     }
 
